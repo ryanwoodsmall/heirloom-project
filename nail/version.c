e140fd457ff801c3270d681851ee926088094440
gritter <>
2004-11-10 21:56:02 +0000

    11.15 release

--- nail/version.c
+++ nail/version.c
@@ -1,4 +1,4 @@
-#define	V	"11.15pre"
+#define	V	"11.15"
 /*
  * Nail - a mail user agent derived from Berkeley Mail.
  *
@@ -39,7 +39,7 @@
 
 #ifndef lint
 #ifdef	DOSCCS
-static char sccsid[] = "@(#)version.c	2.260 (gritter) 11/8/04";
+static char sccsid[] = "@(#)version.c	2.261 (gritter) 11/10/04";
 #endif
 #endif /* not lint */
 
@@ -48,11 +48,11 @@ static char sccsid[] = "@(#)version.c	2.260 (gritter) 11/8/04";
  * Load this file first to get a "total" Mail version.
  */
 /*char	*version = "8.1 6/6/93";*/
-const char *version = "nail " V " 11/8/04";
+const char *version = "nail " V " 11/10/04";
 #ifndef	lint
 static const char *versionid
 #ifdef	__GNUC__
 __attribute__ ((unused))
 #endif
-= "@(#)nail " V " 11/8/04";
+= "@(#)nail " V " 11/10/04";
 #endif	/* !lint */
