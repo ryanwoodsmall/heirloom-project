9b3f61c2e2077127b5d64ba1ad7416b080862477
gritter <>
2005-07-17 12:11:25 +0000

    * Some issues with building on AIX have been fixed.

--- heirloom/Makefile.mk
+++ heirloom/Makefile.mk
@@ -30,41 +30,55 @@ directories:
 links:
 	for i in install ; \
 	do \
+		rm -f $(ROOT)$(DEFBIN)/$$i; \
 		sh build/crossln $(ROOT)$(UCBBIN)/$$i $(ROOT)$(DEFBIN)/$$i $(ROOT); \
 	done
 	for i in basename chmod cp date du echo file id ln mkdir mv nohup pr ps rm rmdir sort test touch tr wc who; \
 	do \
+		rm -f $(ROOT)$(SU3BIN)/$$i; \
 		sh build/crossln $(ROOT)$(SUSBIN)/$$i $(ROOT)$(SU3BIN)/$$i $(ROOT); \
 	done
 	for i in csplit date egrep fgrep find getconf grep id mkdir nawk pg; \
 	do \
+		rm -f $(ROOT)$(S42BIN)/$$i; \
 		sh build/crossln $(ROOT)$(SUSBIN)/$$i $(ROOT)$(S42BIN)/$$i $(ROOT); \
 	done
 	for i in basename chmod du echo file lc ls more nohup od page pax pr rm rmdir sort test touch tr who; \
 	do \
+		rm -f $(ROOT)$(S42BIN)/$$i; \
 		sh build/crossln $(ROOT)$(SV3BIN)/$$i $(ROOT)$(S42BIN)/$$i $(ROOT); \
 	done
 	for i in pax; \
 	do \
+		rm -f $(ROOT)$(SUSBIN)/$$i; \
 		sh build/crossln $(ROOT)$(SV3BIN)/$$i $(ROOT)$(SUSBIN)/$$i $(ROOT); \
 	done
 	for i in getconf; \
 	do \
+		rm -f $(ROOT)$(SV3BIN)/$$i; \
 		sh build/crossln $(ROOT)$(SUSBIN)/$$i $(ROOT)$(SV3BIN)/$$i $(ROOT); \
 	done
 	for i in od; \
 	do \
+		rm -f $(ROOT)$(SU3BIN)/$$i; \
 		sh build/crossln $(ROOT)$(SV3BIN)/$$i $(ROOT)$(SU3BIN)/$$i $(ROOT); \
 	done
 	for i in apropos expand hostname man printenv renice tcopy ul unexpand uptime users w whoami whatis; \
 	do \
+		rm -f $(ROOT)$(UCBBIN)/$$i; \
 		sh build/crossln $(ROOT)$(DEFBIN)/$$i $(ROOT)$(UCBBIN)/$$i $(ROOT); \
 	done
+	rm -f $(ROOT)$(UCBBIN)/more
 	sh build/crossln $(ROOT)$(SV3BIN)/more $(ROOT)$(UCBBIN)/more $(ROOT)
+	rm -f $(ROOT)$(UCBBIN)/catman
 	sh build/crossln $(ROOT)$(DEFSBIN)/catman $(ROOT)$(UCBBIN)/catman $(ROOT)
+	rm -f $(ROOT)$(SV3BIN)/awk
 	sh build/crossln $(ROOT)$(DEFBIN)/oawk $(ROOT)$(SV3BIN)/awk $(ROOT)
+	rm -f $(ROOT)$(S42BIN)/awk
 	sh build/crossln $(ROOT)$(S42BIN)/nawk $(ROOT)$(S42BIN)/awk $(ROOT)
+	rm -f $(ROOT)$(SUSBIN)/awk
 	sh build/crossln $(ROOT)$(SUSBIN)/nawk $(ROOT)$(SUSBIN)/awk $(ROOT)
+	rm -f $(ROOT)$(SU3BIN)/awk
 	sh build/crossln $(ROOT)$(SU3BIN)/nawk $(ROOT)$(SU3BIN)/awk $(ROOT)
 	rm -f $(ROOT)$(MANDIR)/man1/awk.1
 	if test $(DEFBIN) = $(S42BIN) -o $(DEFBIN) = $(SUSBIN) \
