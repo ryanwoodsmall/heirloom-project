1a7606b7945db6812813b946b72182d60244fa06
gritter <>
2005-08-25 11:06:03 +0000

    * The "-mpictures" macros have been added from Plan 9 sources.

--- heirloom-doctools/troff/troff.d/tmac.d/Makefile.mk
+++ heirloom-doctools/troff/troff.d/tmac.d/Makefile.mk
@@ -2,7 +2,8 @@ MACS = acm.me bib chars.me deltext.me e eqn.me \
 	float.me footnote.me index.me local.me m mmn mmt ms.acc \
 	ms.cov ms.eqn ms.ref ms.tbl ms.ths ms.toc null.me refer.me \
 	s sh.me tbl.me thesis.me tz.map v vgrind \
-	an andoc doc doc-common doc-ditroff doc-nroff doc-syms
+	an andoc doc doc-common doc-ditroff doc-nroff doc-syms \
+	pictures
 
 .SUFFIXES: .in
 .in:
@@ -16,6 +17,8 @@ install: all
 	do \
 		$(INSTALL) -c -m 644 $$i $(ROOT)$(MACDIR)/$$i || exit; \
 	done
+	test -d $(ROOT)$(MANDIR)/man7b || mkdir -p $(ROOT)$(MANDIR)/man7b
+	$(INSTALL) -c -m 644 mpictures.7b $(ROOT)$(MANDIR)/man7b/mpictures.7b
 
 clean:
 	rm -f andoc bib doc e m s
