49c66efb43225a9cb72b2744424982af31495e7a
gritter <>
2005-09-12 23:21:17 +0000

    UTF-8 table added

--- heirloom-doctools/eqn/eqnchar.d/Makefile.mk
+++ heirloom-doctools/eqn/eqnchar.d/Makefile.mk
@@ -1,14 +1,26 @@
-FILES = ascii eqnchar greek iso
+FILES = ascii eqnchar greek iso utf-8
 
-all:
+.c.o:
+	$(CC) $(CFLAGS) $(WARN) $(CPPFLAGS) $(EUC) -c $<
 
-install:
+all: $(FILES)
+
+utf-8: genutf8
+	-./genutf8 >utf-8
+
+genutf8: genutf8.o
+	$(CC) $(LDFLAGS) genutf8.o $(LIBS) -o genutf8
+
+
+install: all
 	test -d $(ROOT)$(PUBDIR) || mkdir -p $(ROOT)$(PUBDIR)
 	for i in $(FILES); \
 	do \
+		test -s $$i || continue; \
 		$(INSTALL) -c -m 644 $$i $(ROOT)$(PUBDIR)/$$i || exit; \
 	done
 
 clean:
+	rm -f utf-8 genutf8 genutf8.o core log *~
 
 mrproper: clean
