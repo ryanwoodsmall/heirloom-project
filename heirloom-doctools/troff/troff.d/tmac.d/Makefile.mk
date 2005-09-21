623fb0bfce76ee0fe65dea293fbfd2f66e873eef
gritter <>
2005-09-21 17:45:32 +0000

    * The "-mcolor" macros have been added from Plan 9 sources.

--- heirloom-doctools/troff/troff.d/tmac.d/Makefile.mk
+++ heirloom-doctools/troff/troff.d/tmac.d/Makefile.mk
@@ -3,7 +3,7 @@ MACS = acm.me bib chars.me deltext.me e eqn.me \
 	ms.cov ms.eqn ms.ref ms.tbl ms.ths ms.toc null.me refer.me \
 	s sh.me tbl.me thesis.me tz.map v vgrind \
 	an andoc doc doc-common doc-ditroff doc-nroff doc-syms \
-	pictures
+	pictures color
 
 .SUFFIXES: .in
 .in:
@@ -18,6 +18,7 @@ install: all
 		$(INSTALL) -c -m 644 $$i $(ROOT)$(MACDIR)/$$i || exit; \
 	done
 	test -d $(ROOT)$(MANDIR)/man7b || mkdir -p $(ROOT)$(MANDIR)/man7b
+	$(INSTALL) -c -m 644 mcolor.7b $(ROOT)$(MANDIR)/man7b/mcolor.7b
 	$(INSTALL) -c -m 644 mpictures.7b $(ROOT)$(MANDIR)/man7b/mpictures.7b
 
 clean:
