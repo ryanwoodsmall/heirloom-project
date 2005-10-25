a9e5e6f229f1e467244c52cecfcd9ed98f297218
gritter <>
2005-10-25 18:55:21 +0000

    ensure that runinv runs in build root

--- heirloom-doctools/refer/papers/runinv.sh
+++ heirloom-doctools/refer/papers/runinv.sh
@@ -6,12 +6,12 @@
 #
 # Portions Copyright (c) 2005 Gunnar Ritter, Freiburg i. Br., Germany
 #
-# Sccsid %W% (gritter) %G%
+# Sccsid @(#)runinv.sh	1.3 (gritter) 10/25/05
 #
 
-PATH=/usr/5bin:$PATH export PATH
+PATH=@REFDIR@:/usr/5bin:$PATH export PATH
 
-@REFDIR@/mkey R* | @REFDIR@/inv -v -h997 -n Xind
+mkey R* | inv -v -h997 -n Xind
 mv Xind.ia Ind.ia
 mv Xind.ib Ind.ib
 mv Xind.ic Ind.ic
