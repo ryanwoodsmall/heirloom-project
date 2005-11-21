153e7dd6d107686f976c3148ff8c5a210f95c3b5
gritter <>
2005-11-21 23:46:15 +0000

    ported to DragonFlyBSD

--- heirloom-doctools/pic/version.c
+++ heirloom-doctools/pic/version.c
@@ -5,8 +5,8 @@
 #else
 #define	USED
 #endif
-static const char sccsid[] USED = "@(#)/usr/ucb/pic.sl	5.1 (gritter) 10/25/05";
-const char version[] = "version July 5, 1993	5.1 (gritter) 10/25/05";
+static const char sccsid[] USED = "@(#)/usr/ucb/pic.sl	5.2 (gritter) 11/22/05";
+const char version[] = "version July 5, 1993	5.2 (gritter) 11/22/05";
 /* SLIST */
 /*
 arcgen.c:	Sccsid @(#)arcgen.c	1.3 (gritter) 10/18/05	
@@ -20,11 +20,9 @@ main.c:	Sccsid @(#)main.c	1.5 (gritter) 10/25/05
 misc.c:	Sccsid @(#)misc.c	1.3 (gritter) 10/18/05	
 movegen.c:	Sccsid @(#)movegen.c	1.2 (gritter) 10/18/05	
 pic.h:	Sccsid @(#)pic.h	1.5 (gritter) 10/25/05	
-picl.c:	Sccsid @(#)picl.l	1.5 (gritter) 10/25/05	
-picl.c: * Sccsid @(#)ncform	1.3 (gritter) 6/18/05
-picl.l:	Sccsid @(#)picl.l	1.5 (gritter) 10/25/05	
+picl.c:	Sccsid @(#)version.c	5.2 (gritter) 11/22/05	
+picl.l:	Sccsid @(#)picl.l	1.6 (gritter) 11/22/05	
 picy.c:	Sccsid @(#)picy.y	1.3 (gritter) 10/18/05	
-picy.c: * Sccsid @(#)yaccpar	1.4 (gritter) 7/30/05
 picy.y:	Sccsid @(#)picy.y	1.3 (gritter) 10/18/05	
 pltroff.c:	Sccsid @(#)pltroff.c	1.2 (gritter) 10/18/05	
 print.c:	Sccsid @(#)print.c	1.3 (gritter) 10/18/05	
