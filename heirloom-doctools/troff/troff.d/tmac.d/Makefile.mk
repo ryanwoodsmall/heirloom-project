41d9a7f6317a433186eebdc839e8f92419e34b97
gritter <>
2006-09-03 19:31:44 +0000

    Removed tz.map since it is unreferenced.

--- heirloom-doctools/troff/troff.d/tmac.d/Makefile.mk
+++ heirloom-doctools/troff/troff.d/tmac.d/Makefile.mk
@@ -1,7 +1,7 @@
 MACS = acm.me bib chars.me deltext.me e eqn.me \
 	float.me footnote.me index.me local.me m mmn mmt ms.acc \
 	ms.cov ms.eqn ms.ref ms.tbl ms.ths ms.toc null.me refer.me \
-	s sh.me tbl.me thesis.me tz.map v vgrind \
+	s sh.me tbl.me thesis.me v vgrind \
 	an andoc doc doc-common doc-ditroff doc-nroff doc-syms \
 	pictures color pm srefs ptx safe g
 
