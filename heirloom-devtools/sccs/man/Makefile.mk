05015733012b5d1a54df970ea3cb34add65632cd
gritter <>
2007-02-01 13:57:18 +0000

    moved prt to section 1b

--- heirloom-devtools/sccs/man/Makefile.mk
+++ heirloom-devtools/sccs/man/Makefile.mk
@@ -8,7 +8,6 @@ install: all
 	$(INSTALL) -c -m 644 get.1 $(ROOT)$(MANDIR)/man1/get.1
 	$(INSTALL) -c -m 644 help.1 $(ROOT)$(MANDIR)/man1/help.1
 	$(INSTALL) -c -m 644 prs.1 $(ROOT)$(MANDIR)/man1/prs.1
-	$(INSTALL) -c -m 644 prt.1 $(ROOT)$(MANDIR)/man1/prt.1
 	$(INSTALL) -c -m 644 rmdel.1 $(ROOT)$(MANDIR)/man1/rmdel.1
 	$(INSTALL) -c -m 644 sact.1 $(ROOT)$(MANDIR)/man1/sact.1
 	$(INSTALL) -c -m 644 sccsdiff.1 $(ROOT)$(MANDIR)/man1/sccsdiff.1
@@ -16,6 +15,8 @@ install: all
 	$(INSTALL) -c -m 644 val.1 $(ROOT)$(MANDIR)/man1/val.1
 	$(INSTALL) -c -m 644 vc.1 $(ROOT)$(MANDIR)/man1/vc.1
 	$(INSTALL) -c -m 644 what.1 $(ROOT)$(MANDIR)/man1/what.1
+	test -d $(ROOT)$(MANDIR)/man1b || mkdir -p $(ROOT)$(MANDIR)/man1b
+	$(INSTALL) -c -m 644 prt.1b $(ROOT)$(MANDIR)/man1b/prt.1b
 	test -d $(ROOT)$(MANDIR)/man5 || mkdir -p $(ROOT)$(MANDIR)/man5
 	$(INSTALL) -c -m 644 sccsfile.5 $(ROOT)$(MANDIR)/man5/sccsfile.5
 
